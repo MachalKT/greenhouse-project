b32e387d5e59329ad2c34cc883a88d7ac10dd8a6
Maciej Halat <halat.maciej.97@gmail.com>
2025-03-01 17:04:29 +0100

    improved msToUs and sToMs

--- common/utils.hpp
+++ common/utils.hpp
@@ -7,14 +7,14 @@ namespace utils {
  * @brief Convert milliseconds to microseconds.
  */
 template <typename TReturn, typename T> constexpr TReturn msToUs(T numberMs) {
-  return static_cast<TReturn>(numberMs) * static_cast<TReturn>(1000);
+  return static_cast<TReturn>(numberMs * static_cast<TReturn>(1000));
 }
 
 /**
  * @brief Convert seconds to milliseconds.
  */
 template <typename TReturn, typename T> constexpr TReturn sToMs(T numberS) {
-  return static_cast<TReturn>(numberS) * static_cast<TReturn>(1000);
+  return static_cast<TReturn>(numberS * static_cast<TReturn>(1000));
 }
 
 } // namespace utils

56f9556a200e91183ec75bdbf4e656e9ee059782
Maciej Halat <halat.maciej.97@gmail.com>
2025-03-14 10:15:24 +0100

    added led management

--- application/inc/uithread.hpp
+++ application/inc/uithread.hpp
@@ -5,7 +5,9 @@
 #include "iled.hpp"
 #include "queue.hpp"
 #include "threadbase.hpp"
+#include "timer.hpp"
 #include "types.hpp"
+#include "utils.hpp"
 
 namespace app {
 
@@ -22,6 +24,7 @@ class UiThread final : public sw::ThreadBase {
         ui::Button& button;
         ui::ILed& led;
         sw::IQueueReceiver<def::ui::LedEvent>& queue;
+        timer::ITimer& ledTimer;
     };
 
     /**
@@ -49,10 +52,17 @@ class UiThread final : public sw::ThreadBase {
      */
     void handleLedEvent_(def::ui::LedEvent& event);
 
+    void setLedTimer_();
+
+    static constexpr common::Time DISCONNECTED_LED_TIME_US{
+        common::utils::msToUs<common::Time, common::Time>(
+            common::utils::sToMs<common::Time, common::Time>(2))};
     static constexpr uint32_t STACK_DEPTH{2048};
     static constexpr int PRIORITY{5};
     static constexpr sw::ThreadBase::CoreId CORE_ID{sw::ThreadBase::CoreId::_0};
     Config config_;
+    def::ui::LedEvent radioLedEvent_{def::ui::LedEvent::UNKNOWN};
+    def::ui::LedEvent wifiLedEvent_{def::ui::LedEvent::UNKNOWN};
 };
 
 } // namespace app
